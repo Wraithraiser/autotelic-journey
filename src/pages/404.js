cb2e4c6ec2bb694a3f46f56287f77a2e2c8e33e3
Alexandre Lim <alexandre.lim@abbeal.com>
2021-01-03 20:13:09 +0100

    feat(pages): improve pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,31 +4,27 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 
-class NotFoundPage extends React.Component {
-  render() {
-    const { data } = this.props;
-    const siteTitle = data.site.siteMetadata.title;
+const NotFoundPage = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata.title;
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>
-          You just hit a route that doesn&#39;t exist... but let's just be Happy
-          !
-        </p>
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/ZbZSe6N_BXs"
-          frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullscreen
-        />
-      </Layout>
-    );
-  }
-}
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title="404: Not Found" />
+      <h1>Not Found</h1>
+      <p>
+        You just hit a route that doesn&#39;t exist... but let's just be Happy !
+      </p>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/ZbZSe6N_BXs"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowFullscreen
+      />
+    </Layout>
+  );
+};
 
 export default NotFoundPage;
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,48 +8,79 @@ import Footer from '../components/Footer';
 import { rhythm } from '../utils/typography';
 import { formatReadingTime } from '../utils/helpers';
 
-class BlogIndex extends React.Component {
-  render() {
-    const { data } = this.props;
-    const siteTitle = data.site.siteMetadata.title;
-    const posts = data.allMarkdownRemark.edges;
+const BlogIndex = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`;
+  const posts = data.allMarkdownRemark.nodes;
 
+  if (posts.length === 0) {
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title="Alexandre Lim"
-          keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-        />
+      <Layout location={location} title={siteTitle}>
+        <SEO title="All posts" />
         <Bio />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug;
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  fontFamily: 'Montserrat, sans-serif',
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>
-                {node.frontmatter.date}
-                {` • ${formatReadingTime(node.timeToRead)}`}
-              </small>
-              <p
-                dangerouslySetInnerHTML={{ __html: node.frontmatter.spoiler }}
-              />
-            </div>
-          );
-        })}
-        <Footer />
+        <p>
+          No blog posts found. Add markdown posts to "content/blog" (or the
+          directory you specified for the "gatsby-source-filesystem" plugin in
+          gatsby-config.js).
+        </p>
       </Layout>
     );
   }
-}
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO
+        title="All posts"
+        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+      />
+      <Bio />
+      <ol style={{ listStyle: `none` }}>
+        {posts.map(post => {
+          const title = post.frontmatter.title || post.fields.slug;
+
+          return (
+            <li key={post.fields.slug}>
+              <article
+                className="post-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h3
+                    style={{
+                      fontFamily: 'Montserrat, sans-serif',
+                      marginBottom: rhythm(1 / 4),
+                    }}
+                  >
+                    <Link
+                      style={{ boxShadow: `none` }}
+                      to={post.fields.slug}
+                      itemProp="url"
+                    >
+                      <span itemProp="headline">{title}</span>
+                    </Link>
+                  </h3>
+                  <small>
+                    {post.frontmatter.date}
+                    {` • ${formatReadingTime(post.timeToRead)}`}
+                  </small>
+                </header>
+                <section>
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: post.frontmatter.spoiler || post.excerpt,
+                    }}
+                    itemProp="spoiler"
+                  />
+                </section>
+              </article>
+            </li>
+          );
+        })}
+      </ol>
+      <Footer />
+    </Layout>
+  );
+};
 
 export default BlogIndex;
 
@@ -61,17 +92,16 @@ export const pageQuery = graphql`
       }
     }
     allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          fields {
-            slug
-          }
-          timeToRead
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            spoiler
-          }
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        timeToRead
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          spoiler
         }
       }
     }
